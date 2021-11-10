35a35273d78830dba448273c6b4c51b9ea12ea3c
Johnny Jeong <ksjeong@gmail.com>
2021-11-10 23:49:37 +1100

    first commit

---
+++ src/main/java/com/gugi/allselladmin/AllsellAdminApplication.java
@@ -0,0 +1,13 @@
+package com.gugi.allselladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllsellAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllsellAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gugi/allselladmin/AllsellAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gugi.allselladmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllsellAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
